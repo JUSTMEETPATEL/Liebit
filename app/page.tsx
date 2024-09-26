77c614668ca11d5bc80452d7073fb725869a4fd8
JUSTMEETPATEL <justmeetpatel@gmail.com>
2024-09-26 19:59:37 +0530

    Landing Page 1.0

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-// import Image from "next/image";
+// import { ProductShowcaseComponent } from "@/components/components-product-showcase";
+import { LandingPage } from "@/components/LandingPage";
 
 
 export default function Home() {
   return (
     <>
 
-    </>  
+    {/* <ProductShowcaseComponent /> */}
+    <LandingPage />
+    </>
   );
 }
