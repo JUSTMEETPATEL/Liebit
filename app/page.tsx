d2f361adb7787215377560941e643140883106ab
JUSTMEETPATEL <justmeetpatel@gmail.com>
2024-09-26 20:02:52 +0530

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-// import { ProductShowcaseComponent } from "@/components/components-product-showcase";
 import { LandingPage } from "@/components/LandingPage";
-
 
 export default function Home() {
   return (
     <>
-
-    {/* <ProductShowcaseComponent /> */}
     <LandingPage />
     </>
   );
